a56071cd8fb43f4654b25624d4c6d50383eec231
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-01-14 00:11:50 +0500

    Js docs for DefaultEqualityComparer were added

--- src/defaultEqualityComparer.js
+++ src/defaultEqualityComparer.js
@@ -2,6 +2,17 @@ const { equals } = require("./equals");
 const { getHashCode } = require("./getHashCode");
 
 module.exports.DefaultEqualityComparer = {
+  /**
+   * Method checks two objects equality.
+   * @param {*} first First object.
+   * @param {*} second Second object.
+   * @returns {boolean} Comparison result.
+   */
   equals: (firstOperand, secondOperand) => equals(firstOperand, secondOperand),
+  /**
+   * Method calculates object hashcode.
+   * @param {*} operand Any object.
+   * @returns {number} Computed hashcode.
+   */
   getHashCode: operand => getHashCode(operand)
 };
